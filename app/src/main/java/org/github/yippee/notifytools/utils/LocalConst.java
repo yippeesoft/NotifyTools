7e2cdfe857215d7a830b4ec9730c5f02afb807c6
yippeesoft <yippeesoft@gmail.com>
2017-01-06 14:54:18 +0800

    add heweather key

---
+++ app/src/main/java/org/github/yippee/notifytools/utils/LocalConst.java
@@ -0,0 +1,10 @@
+package org.github.yippee.notifytools.utils;
+
+/**
+ * Created by sf on 2017/1/6.
+ */
+
+public class LocalConst {
+    //http://console.heweather.com/my/service
+    public static String heweatherKey="123123";
+}
