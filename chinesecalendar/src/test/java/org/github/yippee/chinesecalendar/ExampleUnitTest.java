9f3d6c0d63be6519ec9e59c7dc4963600c5fe8a6
yippeesoft <yippeesoft@gmail.com>
2017-03-01 15:24:06 +0800

    农历 测试 增加测试结果注释；顺便尝试解决github 总是显示 Latest commit  just now

--- chinesecalendar/src/test/java/org/github/yippee/chinesecalendar/ExampleUnitTest.java
+++ chinesecalendar/src/test/java/org/github/yippee/chinesecalendar/ExampleUnitTest.java
@@ -31,5 +31,18 @@ public class ExampleUnitTest {
         System.out.println("星期：" + cc.WeekDayStr());
         System.out.println("星宿：" + cc.ChineseConstellation());
         System.out.println("星座：" + cc.Constellation());
+
+//        AA阳历：公元Wed Mar 01 15:22:38 CST 2017
+//        AA属相：狗
+//        AA农历：农历二零一七年二月初四
+//        AA时辰：戊申
+//        节气：
+//        前一个节气：雨水[2017-02-18]
+//        后一个节气：惊蛰[2017-03-05]
+//        节日：国际海豹日
+//        干支：丁酉年癸卯月丁巳日
+//        星期：星期三
+//        星宿：壁水獝
+//        星座：双鱼座
     }
 }
