787ad35ea2c3abe59a4adf38e015f7cb52ba5fd1
yippeesoft <yippeesoft@gmail.com>
2017-08-16 16:17:52 +0800

    china-poem 测试直接反射和beanutils效率 5964:7064

--- china-poem/src/main/java/org/github/yippee/china_poem/MainActivity.java
+++ china-poem/src/main/java/org/github/yippee/china_poem/MainActivity.java
@@ -15,7 +15,12 @@ import android.view.MenuItem;
 import com.jakewharton.rxrelay2.BehaviorRelay;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.PropertyUtils;
 import org.github.yippee.china_poem.Utils.LogUtils;
+import org.github.yippee.china_poem.poem2db.bean.Tangshi;
 import org.github.yippee.china_poem.view.DataBuilder;
 import org.github.yippee.china_poem.view.Rx.RxView;
 import org.github.yippee.china_poem.view.ViewBuilder;
@@ -50,7 +55,68 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     //Ts2Sqlite.main(new String[]{});
     //Ts2Sqlite.getAuthors(this);
-    new RxView(this).init();
+    new Thread(new Runnable() {
+      @Override public void run() {
+        long start=System.currentTimeMillis();
+
+        String s;
+        try {
+        Class<?> managerClass = Class.forName("org.github.yippee.china_poem.poem2db.bean.Tangshi");
+        Object ts = managerClass.newInstance();
+        for(int i=0;i<100*100*100;i++) {
+          //Tangshi ts = new Tangshi();
+
+            PropertyUtils.setSimpleProperty(ts, "title", "AAAAAAAAAA");
+            s=((String) PropertyUtils.getSimpleProperty(ts, "title"));
+
+
+        } } catch (IllegalAccessException e) {
+          e.printStackTrace();
+        } catch (InvocationTargetException e) {
+          e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+          e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+          e.printStackTrace();
+        } catch (InstantiationException e) {
+          e.printStackTrace();
+        }
+        log.e("耗时："+(System.currentTimeMillis()-start));  //7064
+
+          start=System.currentTimeMillis();
+          //ts = new Tangshi();
+          s="";
+        try {
+        Class<?> managerClass = Class.forName("org.github.yippee.china_poem.poem2db.bean.Tangshi");
+        Object obj = managerClass.newInstance();
+        for(int i=0;i<100*100*100;i++) {
+
+
+
+            Field field=managerClass.getDeclaredField("title");
+            field.setAccessible(true);
+            field.set(obj,"AAAAAAAAAA");
+            //PropertyUtils.setSimpleProperty(ts, "title", "title");
+            Field field2=managerClass.getDeclaredField("title");
+            field.setAccessible(true);
+            s=   (String) field.get(obj); //((String) PropertyUtils.getSimpleProperty(ts, "title"));
+            //log.e("ss "+s);
+
+        } } catch (IllegalAccessException e) {
+          e.printStackTrace();
+        }   catch (NoSuchFieldException e) {
+          e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+          e.printStackTrace();
+        } catch (InstantiationException e) {
+          e.printStackTrace();
+        }
+        log.e("耗时2："+(System.currentTimeMillis()-start));  //5964
+
+      }
+    }).start();
+
+    //new RxView(this).init();
   }
 
   @Override public void onBackPressed() {
