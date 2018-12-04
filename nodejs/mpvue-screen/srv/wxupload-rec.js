8b1d8c979e614cf174f84c07c604418bc98b301e
yippeesoft <yippeesoft@gmail.com>
2018-12-04 16:07:17 +0800

    PREF nodejs mpvue-screen srv 参照 https://www.cnblogs.com/xiaotaiyang/p/6090037.html 增加接收

---
+++ nodejs/mpvue-screen/srv/wxupload-rec.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: './'}).array('image'));
+
+app.get('/index.html', function (req, res) {
+  console.log(req);  // 上传的文件信息
+   res.sendFile( __dirname + "/" + "index.htm" );
+})
+
+app.post('/file_upload', function (req, res) {
+
+  console.log(req.files[0]);  // 上传的文件信息
+
+  var des_file = __dirname + "/" + req.files[0].originalname;
+  fs.readFile( req.files[0].path, function (err, data) {
+       fs.writeFile(des_file, data, function (err) {
+        if( err ){
+             console.log( err );
+        }else{
+              response = {
+                  message:'File uploaded successfully', 
+                  filename:req.files[0].originalname
+             };
+         }
+         console.log( response );
+         res.end( JSON.stringify( response ) );
+      });
+  });
+})
+
+var server = app.listen(8081, function () {
+
+ var host = server.address().address
+ var port = server.address().port
+
+ console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+})
+
+// router.post("/api/upload", async (req, res) => {  
+//   let upload = multer(uploadCfg).any();  
+//   upload(req, res, async (err) => {  
+//     if (err) {  
+//       res.json({ path: `//uploads/tmp/${uploadFile.filename}` });  
+//       console.log(err);  
+//       return;  
+//     };  
+//     console.log(req.files);  
+//     let uploadFile = req.files[0];  
+//     res.json({ path: `//uploads/tmp/${uploadFile.filename}` });  
+//   });  
+// })
