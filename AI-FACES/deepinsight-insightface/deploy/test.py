bfda32b5d08b26cfa6fb1a35d96a0b706be7da01
yippeesoft <yippeesoft@gmail.com>
2019-08-13 13:57:30 +0800

    ai-face  https://github.com/deepinsight/insightface test.py win下初步运行

--- AI-FACES/deepinsight-insightface/deploy/test.py
+++ AI-FACES/deepinsight-insightface/deploy/test.py
@@ -7,7 +7,7 @@ import numpy as np
 parser = argparse.ArgumentParser(description='face model test')
 # general
 parser.add_argument('--image-size', default='112,112', help='')
-parser.add_argument('--model', default='', help='path to load model.')
+parser.add_argument('--model', default='../models/model,0', help='path to load model.')
 parser.add_argument('--ga-model', default='', help='path to load model.')
 parser.add_argument('--gpu', default=0, type=int, help='gpu id')
 parser.add_argument('--det', default=0, type=int, help='mtcnn option, 1 means using R+O, 0 means detect from begining')
@@ -16,20 +16,26 @@ parser.add_argument('--threshold', default=1.24, type=float, help='ver dist thre
 args = parser.parse_args()
 
 model = face_model.FaceModel(args)
-img = cv2.imread('Tom_Hanks_54745.png')
+img = cv2.imread('X:\pic\ll2.jpg')
+
+
 img = model.get_input(img)
-#f1 = model.get_feature(img)
-#print(f1[0:10])
-gender, age = model.get_ga(img)
-print(gender)
-print(age)
-sys.exit(0)
-img = cv2.imread('/raid5data/dplearn/megaface/facescrubr/112x112/Tom_Hanks/Tom_Hanks_54733.png')
+
+f1 = model.get_feature(img)
+print(f1[0:10])
+#gender, age = model.get_ga(img)
+#print(gender)
+#print(age)
+#sys.exit(0)
+img = cv2.imread('X:\pic\ll2.jpg')
+#cv2.imshow('ss',img)
+img = model.get_input(img)
 f2 = model.get_feature(img)
 dist = np.sum(np.square(f1-f2))
 print(dist)
 sim = np.dot(f1, f2.T)
 print(sim)
+cv2.waitKey(0)
 #diff = np.subtract(source_feature, target_feature)
 #dist = np.sum(np.square(diff),1)
 
