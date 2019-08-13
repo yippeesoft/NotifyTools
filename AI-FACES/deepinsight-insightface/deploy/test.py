40c5da107337623c0223338c3d5f332c62bb4593
yippeesoft <yippeesoft@gmail.com>
2019-08-13 17:19:53 +0800

    ai-face  insightface test.py lfw测试结果 数据来源 https://github.com/deepinsight/insightface/tree/master/recognition/data/lfw 0.9668

--- AI-FACES/deepinsight-insightface/deploy/test.py
+++ AI-FACES/deepinsight-insightface/deploy/test.py
@@ -17,57 +17,95 @@ args = parser.parse_args()
 
 model = face_model.FaceModel(args)
 
-lfw_root='Y:\\tmp\mobileFacenet-ncnn-honghuCode\lfw-112X112\lfw-112X112\\'
-pairs_txt='Y:\\tmp\mobileFacenet-ncnn-honghuCode\pairs_1.txt'
-file=open(pairs_txt)
+def testlfw():
+	lfw_root='X:\\face-data\lfw\\'
+	pairs_txt='.\pairs_label.txt'
+	file=open(pairs_txt)
 
-fout=open('y:\\temp\lfw_out.csv','w+')
+	fout=open('y:\\temp\lfw_insi_test.csv','w+')
 
-outlines=''
-lines=file.readlines()
-file.close()
+	outlines=''
+	lines=file.readlines()
+	file.close()
 
-for line in lines:
-	try:
-		path = line.strip('\n').split(',')
+	for line in lines:
+		try:
+			path = line.strip('\n').split('\t')
 		
-		img = cv2.imread(lfw_root+path[0])
-		img = model.get_input(img)
-		f1 = model.get_feature(img)
+			img = cv2.imread(lfw_root+path[0])
+			img = model.get_input(img)
+			f1 = model.get_feature(img)
 
-		img = cv2.imread(lfw_root+path[1])
-		img = model.get_input(img)
-		f2 = model.get_feature(img)
-		sim = np.dot(f1, f2.T)
-		outlines=outlines+line.strip('\n').strip('\r')+','+str(sim).strip('\n')+'\n'
-	except:
-		print(lfw_root+path[0])
+			img = cv2.imread(lfw_root+path[1])
+			img = model.get_input(img)
+			f2 = model.get_feature(img)
+			sim = np.dot(f1, f2.T)
+			outlines=outlines+line.strip('\n').strip('\r')+','+str(sim).strip('\n')+'\n'
+		except:
+			print(lfw_root+path[0])
 
-fout.write(outlines)
-print('end!!!!!')
-cv2.waitKey()
-sys.exit(0)
+	fout.write(outlines)
+	print('end!!!!!')
+	cv2.waitKey()
+	sys.exit(0)
 
-img = cv2.imread('X:\pic\ll2.jpg')
+def testlfw112():
+	lfw_root='Y:\\tmp\mobileFacenet-ncnn-honghuCode\lfw-112X112\lfw-112X112\\'
+	pairs_txt='Y:\\tmp\mobileFacenet-ncnn-honghuCode\pairs_1.txt'
+	file=open(pairs_txt)
+
+	fout=open('y:\\temp\lfw_out.csv','w+')
+
+	outlines=''
+	lines=file.readlines()
+	file.close()
+
+	for line in lines:
+		try:
+			path = line.strip('\n').split(',')
+		
+			img = cv2.imread(lfw_root+path[0])
+			img = model.get_input(img)
+			f1 = model.get_feature(img)
+
+			img = cv2.imread(lfw_root+path[1])
+			img = model.get_input(img)
+			f2 = model.get_feature(img)
+			sim = np.dot(f1, f2.T)
+			outlines=outlines+line.strip('\n').strip('\r')+','+str(sim).strip('\n')+'\n'
+		except:
+			print(lfw_root+path[0])
+
+	fout.write(outlines)
+	print('end!!!!!')
+	cv2.waitKey()
+	sys.exit(0)
+
+def old_src():
+	img = cv2.imread('X:\pic\ll2.jpg')
 
 
-img = model.get_input(img)
+	img = model.get_input(img)
 
-f1 = model.get_feature(img)
-print(f1[0:10])
-#gender, age = model.get_ga(img)
-#print(gender)
-#print(age)
-#sys.exit(0)
-img = cv2.imread('X:\pic\ll2.jpg')
-#cv2.imshow('ss',img)
-img = model.get_input(img)
-f2 = model.get_feature(img)
-dist = np.sum(np.square(f1-f2))
-print(dist)
-sim = np.dot(f1, f2.T)
-print(sim)
-cv2.waitKey(0)
-#diff = np.subtract(source_feature, target_feature)
-#dist = np.sum(np.square(diff),1)
+	f1 = model.get_feature(img)
+	print(f1[0:10])
+	#gender, age = model.get_ga(img)
+	#print(gender)
+	#print(age)
+	#sys.exit(0)
+	img = cv2.imread('X:\pic\ll2.jpg')
+	#cv2.imshow('ss',img)
+	img = model.get_input(img)
+	f2 = model.get_feature(img)
+	dist = np.sum(np.square(f1-f2))
+	print(dist)
+	sim = np.dot(f1, f2.T)
+	print(sim)
+	cv2.waitKey(0)
+	#diff = np.subtract(source_feature, target_feature)
+	#dist = np.sum(np.square(diff),1)
 
+if __name__ == "__main__":
+	print('main begin')
+	testlfw()
+	print('main end')
