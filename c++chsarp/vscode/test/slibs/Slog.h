40d4b4d8a7049b25b209052a9746f1814ba50b86
yippeesoft <yippeesoft@gmail.com>
2020-10-20 16:04:36 +0800

    PREF c++ vscode test slibs/slog  增加   export a DLL API for Windows and still support Linux:

--- c++chsarp/vscode/test/slibs/Slog.h
+++ c++chsarp/vscode/test/slibs/Slog.h
@@ -1,3 +1,21 @@
 #pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef _WIN32
+#  ifdef MODULE_API_EXPORTS
+#    define MODULE_API __declspec(dllexport)
+#  else
+#    define MODULE_API __declspec(dllimport)
+#  endif
+#else
+#  define MODULE_API
+#endif
 #include <stdio.h>
-__declspec(dllexport) void testLibs();
+	MODULE_API void testLibs();
+
+#ifdef __cplusplus
+}
+#endif
+
