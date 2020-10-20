2ee22003061de17fc0dd2c0361bacc43347e8e87
yippeesoft <yippeesoft@gmail.com>
2020-10-20 15:59:39 +0800

    PREF c++ vscode test 增加slibs；增加vs2017编译设置

---
+++ c++chsarp/vscode/test/slibs/Slog.cpp
@@ -0,0 +1,6 @@
+#include "Slog.h"
+
+__declspec(dllexport) void testLibs()
+{
+	printf("test lib cmake %d\n", 1);
+}
---
+++ c++chsarp/vscode/test/slibs/Slog.h
@@ -0,0 +1,3 @@
+#pragma once
+#include <stdio.h>
+__declspec(dllexport) void testLibs();
