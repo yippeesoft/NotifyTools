4fce80caff883b311448fa80740bba831dbb51f9
yippeesoft <yippeesoft@gmail.com>
2020-11-18 17:59:30 +0800

    cplussharp vscode qttest 绘制背景图

--- cplussharp/vscode/qt/qttest/MainWidget1.h
+++ cplussharp/vscode/qt/qttest/MainWidget1.h
@@ -2,16 +2,18 @@
 
 #include <QPushButton>
 #include <QWidget>
-class MainWidget1 : public QWidget {
-  Q_OBJECT
+class MainWidget1 : public QWidget
+{
+    Q_OBJECT
 public:
-  explicit MainWidget1(QWidget *parent = nullptr);
-  void sub_Send(void);
+    explicit MainWidget1(QWidget* parent = nullptr);
+    void sub_Send(void);
+    void paintEvent(QPaintEvent* p);
 
 private:
-  QPushButton *MainButton_1;
+    QPushButton* MainButton_1;
 signals:
 
-  void Send_Open(void);
+    void Send_Open(void);
 public slots:
 };
