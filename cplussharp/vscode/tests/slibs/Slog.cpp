694a70940ffdbb5dc307a9a8f2f88bc0af12ed47
yippeesoft <yippeesoft@gmail.com>
2021-01-12 14:49:45 +0800

    linux c test sigsegv

--- cplussharp/vscode/tests/slibs/Slog.cpp
+++ cplussharp/vscode/tests/slibs/Slog.cpp
@@ -7,6 +7,44 @@
 #include <unistd.h>
 #include <iostream>
 auto console = spdlog::stdout_color_mt("console");
+
+#include <iostream>
+#include <signal.h>
+#include <execinfo.h>
+
+/* include <execinfo.h> to use this macro */
+#define DBG_ASSERT(x)                                                                                 \
+    do                                                                                                \
+    {                                                                                                 \
+        if (x) { break; }                                                                             \
+        std::cout << "###### file:" << __FILE__ << " line:" << __LINE__ << " ######" << std::endl;    \
+        void* pptrace_raw[32] = {0};                                                                  \
+        char** pptrace_str = NULL;                                                                    \
+        int trace_num = 0, iloop = 0;                                                                 \
+        trace_num = backtrace(pptrace_raw, 32);                                                       \
+        pptrace_str = (char**)backtrace_symbols(pptrace_raw, trace_num);                              \
+        for (iloop = 0; iloop < trace_num; iloop++) { std::cout << pptrace_str[iloop] << std::endl; } \
+        if (pptrace_str) { delete pptrace_str; }                                                      \
+    } while (0);
+
+void sigsegv_test()
+{
+    std::cout << __func__ << " begin" << std::endl;
+    char* buff = NULL;
+    buff[1] = buff[1]; /* will crash here */
+    std::cout << __func__ << " end" << std::endl;
+}
+
+void sigsegvhandle(int signo)
+{
+    std::cout << "sigsegvhandle received signal: " << signo << std::endl;
+    /* output callstack */
+    DBG_ASSERT(0);
+    /* reset signal handle to default */
+    signal(signo, SIG_DFL);
+    /* will receive SIGSEGV again and exit app */
+}
+
 void testLibs(std::string ss)
 {
     printf("test lib cmake %d\n", 1);
@@ -18,25 +56,31 @@ void testLibs(std::string ss)
     sprintf(cmd, "pstack %d ", pid);
     // system(cmd);
 }
-
+const int a[] = {SIGHUP, SIGINT, SIGQUIT, SIGILL, SIGTRAP, SIGABRT, SIGFPE, SIGKILL, SIGSEGV};
 int main()
 {
+    for (unsigned int i = 0; i < sizeof(a) / sizeof(int); i++)
+    {
+        signal(a[i], sigsegvhandle);
+    }
     console->enable_backtrace(32); // Store the latest 32 messages in a buffer. Older messages will be dropped.
     // or my_logger->enable_backtrace(32)..
     for (int i = 0; i < 100; i++)
     {
         spdlog::debug("Backtrace message {}", i); // not logged yet..
     }
+
     // e.g. if some error happened:
- 
-    
+
     try
     {
         console->set_level(spdlog::level::debug);
         printf("test liss cmake %d\n", 1);
         // spdlog::get(SPDLOG_NAME)->info("test2"); //
         spdlog::debug("Welcome to spdlog!");
-        int kk = 100 / 0;
+        sigsegv_test();
+        int i = 100 / 0;
+        printf("test lib cmake %d\n", i);
         console->dump_backtrace(); // log them now! show the last 32 messages
         SPDLOG_DEBUG("test5");
         testLibs("sssssss");
