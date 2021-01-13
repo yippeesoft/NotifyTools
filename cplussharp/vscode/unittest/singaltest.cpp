d0f4a04c63d9f9f4d92cc7aab992cb6b35f18b67
yippeesoft <yippeesoft@gmail.com>
2021-01-13 16:26:53 +0800

    linux cpp googletest改为submodule

--- cplussharp/vscode/unittest/singaltest.cpp
+++ cplussharp/vscode/unittest/singaltest.cpp
@@ -1,18 +1,22 @@
 #include "SignalHandle.h"
 #include <gtest/gtest.h>
 using namespace sfutils;
+#include <stdlib.h>
+#include <signal.h>
+// #define __built_in_return_address(x) t(x)
 TEST(testCase, test0)
 {
     int i, j, sum;
     sum = 0;
     j = 0;
     i = 6;
-    sum = i / j;
+    // sum = i / j;
     EXPECT_EQ(2 + 3, 5);
 }
 void sigsegv_test()
 {
     std::cout << __func__ << " begin" << std::endl;
+
     char* buff = NULL;
     buff[1] = buff[1]; /* will crash here */
     std::cout << __func__ << " end" << std::endl;
