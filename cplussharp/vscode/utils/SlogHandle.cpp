430eef1a976dc900e7378d36956f3b7c0589f813
yippeesoft <yippeesoft@gmail.com>
2021-01-14 17:41:42 +0800

    linux cpp add slog -> define log4cplus test

---
+++ cplussharp/vscode/utils/SlogHandle.cpp
@@ -0,0 +1,35 @@
+#include "SlogHandle.h"
+namespace sfutils {
+SlogHandle SlogHandle::_instance;
+SlogHandle::SlogHandle(/* args */)
+{
+}
+
+SlogHandle::~SlogHandle()
+{
+}
+SlogHandle& SlogHandle::GetInstance()
+{
+    std::cout << "SlogHandle::GetInstance()" << std::endl;
+    return _instance;
+}
+bool SlogHandle::init()
+{
+#ifdef SFLOG_LOG4CPLUS
+    //第1步：创建ConsoleAppender
+    log4cplus::SharedAppenderPtr appender(new log4cplus::ConsoleAppender());
+
+    //第2步：设置Appender的名称和输出格式（SimpleLayout）
+    appender->setName(LOG4CPLUS_TEXT("console"));
+    appender->setLayout(std::unique_ptr<log4cplus::Layout>(new log4cplus::SimpleLayout));
+
+    //第3步：获得一个Logger实例，并设置其日志输出等级阈值
+    _logger = log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("test"));
+    _logger.setLogLevel(log4cplus::INFO_LOG_LEVEL);
+
+    //第4步：为Logger实例添加ConsoleAppender
+    _logger.addAppender(appender);
+#endif
+    return true;
+}
+} // namespace sfutils
---
+++ cplussharp/vscode/utils/SlogHandle.h
@@ -0,0 +1,44 @@
+#pragma once
+#ifndef SF_SLOGHANDLES_H
+#define SF_SLOGHANDLES_H
+#include <iostream>
+// #define SFLOG_LOG4CPLUS
+#ifdef SFLOG_LOG4CPLUS
+#include <log4cplus/log4cplus.h>
+#define Slog(x) LOG4CPLUS_INFO(SlogHandle::GetInstance().getLogger(), LOG4CPLUS_TEXT(x));
+#else
+#define Slog(x)
+#endif
+namespace sfutils {
+
+class SlogHandle
+{
+public:
+    static SlogHandle& GetInstance();
+
+private:
+    static SlogHandle _instance;
+
+private:
+#ifdef SFLOG_LOG4CPLUS
+    //用Initializer类进行初始化
+    log4cplus::Initializer initializer;
+    log4cplus::Logger _logger;
+
+#endif
+public:
+#ifdef SFLOG_LOG4CPLUS
+    log4cplus::Logger getLogger()
+    {
+        return _logger;
+    };
+#endif
+    bool init();
+
+protected:
+    SlogHandle(/* args */);
+    ~SlogHandle();
+};
+
+} // namespace sfutils
+#endif
