0d37dae0f20e9052148eaff70636d5d9110d05e7
yippeesoft <yippeesoft@gmail.com>
2021-01-18 09:46:02 +0800

    linux c test add log4cplus set root add two logger

--- cplussharp/vscode/utils/SlogHandle.cpp
+++ cplussharp/vscode/utils/SlogHandle.cpp
@@ -20,11 +20,11 @@ bool SlogHandle::init()
     log4cplus::SharedAppenderPtr appender(new log4cplus::ConsoleAppender());
 
     //第2步：设置Appender的名称和输出格式（SimpleLayout）
-    appender->setName(LOG4CPLUS_TEXT("console"));
+    appender->setName(LOG4CPLUS_TEXT("console1111"));
     appender->setLayout(std::unique_ptr<log4cplus::Layout>(new log4cplus::SimpleLayout));
 
     //第3步：获得一个Logger实例，并设置其日志输出等级阈值
-    _logger = log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("test"));
+    _logger = log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("aaaa"));
     _logger.setLogLevel(log4cplus::INFO_LOG_LEVEL);
 
     //第4步：为Logger实例添加ConsoleAppender
