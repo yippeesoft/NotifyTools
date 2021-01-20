3ca2fb6a3e485cdcf33d9fab58e01bcc251c912d
yippeesoft <yippeesoft@gmail.com>
2021-01-20 09:49:35 +0800

    linux cpp libhttp test ssl add log

--- cplussharp/vscode/unittest/shttphandletest.cpp
+++ cplussharp/vscode/unittest/shttphandletest.cpp
@@ -29,7 +29,7 @@ std::string dump_headers(const Headers& headers)
 void helloword(const httplib::Request& req, httplib::Response& rsp)
 {
     printf("httplib server recv a req: %s %s\n ", req.path.c_str(), req.remote_addr.c_str());
-    printf("dump_headers:%s\n",dump_headers(req.headers).c_str());
+    printf("dump_headers:%s\n", dump_headers(req.headers).c_str());
     // std::string str = R "( <html  lang= ><h1> 武汉, 加油！</h1></html>)";
     // std::string s1 = R "a(C:\Users\Administrator\Desktop\RWtest\write.txt)a";
     std::string s= R"foo(
@@ -48,7 +48,7 @@ C:\Users\Administrator\Desktop\RWtes
 void helloword1(const httplib::Request& req, httplib::Response& rsp)
 {
     printf("httplib server recv a req: %s %s\n ", req.path.c_str(), req.remote_addr.c_str());
-    printf("dump_headers:%s\n",dump_headers(req.headers).c_str());
+    printf("dump_headers:%s\n", dump_headers(req.headers).c_str());
     // std::string str = R "( <html  lang= ><h1> 武汉, 加油！</h1></html>)";
     // std::string s1 = R "a(C:\Users\Administrator\Desktop\RWtest\write.txt)a";
     std::string s= R"foo(
@@ -64,7 +64,44 @@ C:\Users\Administrator\Desktop\RWtes
     rsp.set_content(s1, "text/html"); //最后一个参数是正文类型
     rsp.status = 200;
 }
+std::string log(const Request& req, const Response& res)
+{
+    std::string s;
+    char buf[BUFSIZ];
 
+    s += "================================\n";
+
+    snprintf(buf, sizeof(buf), "%s %s %s", req.method.c_str(),
+             req.version.c_str(), req.path.c_str());
+    s += buf;
+
+    std::string query;
+    for (auto it = req.params.begin(); it != req.params.end(); ++it)
+    {
+        const auto& x = *it;
+        snprintf(buf, sizeof(buf), "%c%s=%s",
+                 (it == req.params.begin()) ? '?' : '&', x.first.c_str(),
+                 x.second.c_str());
+        query += buf;
+    }
+    snprintf(buf, sizeof(buf), "%s\n", query.c_str());
+    s += buf;
+
+    s += dump_headers(req.headers);
+
+    s += "--------------------------------\n";
+
+    snprintf(buf, sizeof(buf), "%d %s\n", res.status, res.version.c_str());
+    s += buf;
+    s += dump_headers(res.headers);
+    s += "\n";
+
+    if (!res.body.empty()) { s += res.body; }
+
+    s += "\n";
+
+    return s;
+}
 int main(int argc, char** argv)
 {
 #ifdef CPPHTTPLIB_OPENSSL_SUPPORT
@@ -77,8 +114,16 @@ int main(int argc, char** argv)
         printf("err init!");
     }
     srv.Get("/", helloword1);
-    srv.Get("^/.*$", helloword);
-
+    // srv.Get("^/.*$", helloword);
+    srv.set_error_handler([](const Request& /*req*/, Response& res) {
+        const char* fmt = "<p>Error Status: <span style='color:red;'>%d</span></p>";
+        char buf[BUFSIZ];
+        snprintf(buf, sizeof(buf), fmt, res.status);
+        res.set_content(buf, "text/html");
+    });
+    srv.set_logger([](const Request& req, const Response& res) {
+        printf("%s", log(req, res).c_str());
+    });
     srv.listen("0.0.0.0", 9000);
 
     getchar();
